c748e37bbe7dda28107fd4dff176a399ed462fbe
sivakalpana <sivakalpana1@gmail.com>
2019-07-10 14:17:52 +0530

    commit first change

---
+++ new1/src/new1/Newjava1.java
@@ -0,0 +1,13 @@
+package new1;
+
+public class Newjava1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			System.out.println("hello new java");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
